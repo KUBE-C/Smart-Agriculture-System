ec66808f56798adeed78b89cb1b5f60e233c640a
wind0529sad <135340402+wind0529sad@users.noreply.github.com>
2023-08-18 16:30:28 +0800

    LED

--- HardWare/LED/Led.h
+++ HardWare/LED/Led.h
@@ -4,6 +4,12 @@
 
 #include "stm32f10x.h"
 
-
+ void LED_Init(void);
+ void LED1_ON(void);
+ void LED1_OFF(void);
+ void LED1_Turn(void);
+ void LED2_ON(void);
+ void LED2_OFF(void);
+  void LED2_Turn(void);
 #endif
 
